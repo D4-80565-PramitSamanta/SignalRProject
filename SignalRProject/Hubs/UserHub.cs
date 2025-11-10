e8bcad010229f3a370ff49d72f8d54afe7f82418
pramit <pramit@zentrumhub.com>
2025-11-10 08:25:25 +0530

    basic flow of signal hub server and clinet

---
+++ SignalRProject/Hubs/UserHub.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRProject.Hubs
+{
+    public class UserHub : Hub
+    {
+        public static int TotalViews {  get; set; }
+        public static int TotalUsers {  get; set; }
+
+        public override Task OnConnectedAsync()
+        {
+            TotalUsers++;
+            Clients.All.SendAsync("UpdateTotalUsers", TotalUsers);
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            TotalUsers--;
+            Clients.All.SendAsync("UpdateTotalUsers", TotalUsers);
+            return base.OnDisconnectedAsync(exception);
+        }
+
+
+        public async Task NewWindowLoaded()
+        {
+            TotalViews++;
+
+            // rpc call , call all the clients that totalviews has updated
+            // clients.All comes from Hub
+            // in brancket we pass client side fucntion and pass parameter
+            await Clients.All.SendAsync("UpdateTotalViews", TotalViews);
+        }
+    }
+}
---
+++ SignalRProject/wwwroot/js/usersCount.js
@@ -0,0 +1,31 @@
+﻿// by now signalR has been installed and added in index.html
+// now create connection
+var connectionToUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/usercount").build();
+
+//rcv notificaton from hub
+//define methods here which are called from hub 
+connectionToUserCount.on("UpdateTotalViews", (val) => {
+    var newCountSpan = document.getElementById("totalViewsCount");
+    newCountSpan.innerText = val.toString();
+})
+
+connectionToUserCount.on("UpdateTotalUsers", (val) => {
+    var newCountSpan = document.getElementById("totalUsersCount");
+    newCountSpan.innerText = val.toString();
+})
+
+// if something happen here invoke the hub method 
+function newWindowLoadedOnClinet() {
+    connectionToUserCount.send("NewWindowLoaded");
+}
+
+function fullfilled() {
+    console.log("hub is up and running");
+    newWindowLoadedOnClinet();
+}
+
+function rejected() {
+    console.log("hub is down")
+}
+
+connectionToUserCount.start().then(fullfilled, rejected);
